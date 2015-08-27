d3d979d03124bc341816c9dce9ac617b84d887af
Jennifer Jones <jybjones@gmail.com>
2015-08-27 14:33:34 -0500

    added socket.io dependencies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
-///_________Minimal Express Server lines 3 - 13___________/////
 'use strict';
 
 var express = require('express');
+var socketio = require('socket.io');
 var app = express();
 
 app.use(express.static('client'));
 
-app.listen(3000, function() {
-  console.log('Server listening on port 3000');
+var server = app.listen(3000, function() {
+  console.log('server listening on port 3000');
 });
+
+socketio(server);
+
+///_________Minimal Express Server before adding ANYTHING else___________/////
+
+// 'use strict';
+
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static('client'));
+
+// app.listen(3000, function() {
+//   console.log('Server listening on port 3000');
+// });
